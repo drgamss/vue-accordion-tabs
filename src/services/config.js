232a836ee55ab8b7eb6e88725353dc756d806e21
Dani Gamss <drgamss@yahoo.com>
2018-06-26 22:41:31 +0300

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 // Define style and behaviour of component.
-// uiStyle - Style of component. Either 'list' or 'accordion'.
+// uiStyle - Style of component. Either 'accordion' or 'tabs'.
 // openUnique - Open tabs one at a time or not.
 export default {
   uiStyle: 'tabs',
