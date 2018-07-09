06e3d1161c84c561228ded727cc104557672d75a
Dani Gamss <drgamss@yahoo.com>
2018-07-09 21:30:45 +0300

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 // Define style and behaviour of component.
 // uiStyle - Style of component. Either 'accordion' or 'tabs'.
-// openUnique - Open tabs one at a time or not.
+// openUnique - Open accordion panes one at a time or not (relavent for accordion view).
 export default {
   uiStyle: 'tabs',
   openUnique: true
